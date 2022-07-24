0922e20d4692ca4293fc3a42ded6550f68a362dc
RohitDhuriya <101047406+RohitDhuriya@users.noreply.github.com>
2022-07-24 11:26:42 +0530

    Add files via upload

---
+++ Python worksheet 1 Q no 15.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Write a python program to print the frequency of each of the characters present in a given string?
+
+# In[ ]:
+
+
+str1=input('Enter the string')
+d1=dict()
+for c in str1:
+    d1[c]=d1.get(c,0)+1
+print(d1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
